e2a1e6774761de348ed94889fde78fdfd30ab6a1
Pavan Mishra <pavanmishra@gmail.com>
2010-06-09 19:44:52 +0530

    capistrano recipies for deployment

---
+++ config/deploy/production.rb
@@ -0,0 +1,79 @@
+#############################################################
+#	Application
+#############################################################
+
+set :application, "ads4good"
+set :deploy_to, "/home/app/public_html"
+
+#############################################################
+#	Settings
+#############################################################
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+set :scm_verbose, true
+set :rails_env, "production" 
+
+#############################################################
+#	Servers
+#############################################################
+
+set :user, "app"
+set :domain, "app.ads4good.org"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+#############################################################
+#	Git
+#############################################################
+
+set :scm, :git
+set :branch, "master"
+set :repository, "git@athonlab.unfuddle.com:athonlab/ads4good-frontend.git"
+#set :deploy_via, :remote_cache
+
+#############################################################
+#	Passenger
+#############################################################
+
+namespace :deploy do
+  desc "Create the database yaml file"
+  task :after_update_code do
+    db_config = <<-EOF
+    production:    
+      adapter: mysql
+      encoding: utf8
+      username: app
+      password: ada4gdorg
+      database: app
+      host: localhost
+    EOF
+    
+    put db_config, "#{release_path}/config/database.yml"
+    
+    #########################################################
+    # Uncomment the following to symlink an uploads directory.
+    # Just change the paths to whatever you need.
+    #########################################################
+    
+    # desc "Symlink the upload directories"
+    # task :before_symlink do
+    #   run "mkdir -p #{shared_path}/uploads"
+    #   run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
+    # end
+  
+  end
+    
+  # Restart passenger on deploy
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+  
+end
