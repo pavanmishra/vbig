e53cf1906041b4240c5e7dd32651fe306370eadf
Pavan Mishra <pavanmishra@gmail.com>
2010-06-17 21:02:36 +0530

    corrected the recipients for the message

--- app/models/message_copy.rb
+++ app/models/message_copy.rb
@@ -1,5 +1,5 @@
 class MessageCopy < ActiveRecord::Base
   belongs_to  :message
-  belongs_to  :recipients,  :class_name => 'User'
+  belongs_to  :recipient,  :class_name => 'User'
   delegate    :author, :created_at, :subject, :body, :recipients, :to => :message
 end
