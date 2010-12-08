6e5b067a4d80d2f4b0b2c615e650f3b24cfb2f0d
Pavan Mishra <pavanmishra@gmail.com>
2010-12-08 23:07:14 +0530

    redirecting to contest after voting for organization

--- app/controllers/organization_users_controller.rb
+++ app/controllers/organization_users_controller.rb
@@ -2,14 +2,15 @@ class OrganizationUsersController < ApplicationController
   before_filter :login_required
 
   def help
+
     organization = Organization.find(params[:id])
     unless current_user.helping?(organization)
       current_user.help(organization) 
       flash[:notice] = 'You are now helping this organization'
     else
-        flash[:notice] = 'You are already'
+        flash[:notice] = 'You are already helping this organization'
     end
-    redirect_to organization
+    redirect_to request.referrer =~ /contest/ ? request.referrer : organization
   end
   
 end
