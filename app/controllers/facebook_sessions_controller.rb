30163c1158fc089995456e6e6cf8287c37dfd02d
Pavan Mishra <pavanmishra@gmail.com>
2010-12-28 17:27:41 +0530

    checking parameters returned by facebook

--- app/controllers/facebook_sessions_controller.rb
+++ app/controllers/facebook_sessions_controller.rb
@@ -3,6 +3,7 @@ class FacebookSessionsController < ApplicationController
   include AuthenticatedSystem
 
   def create
+    raise params.inspect
     if verify_fb_cookie_signature
       logger.debug "verified fb cookie signature"
       user_info = get_user_info
